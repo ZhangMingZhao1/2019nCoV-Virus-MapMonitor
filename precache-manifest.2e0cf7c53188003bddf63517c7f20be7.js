39fdf4795e1eecc5d0a47a62b4f9d177fb04c0b4
ZhangMingZhao1 <1104272319@qq.com>
2020-01-21 21:27:30 +0800

    refactor: move build file

---
+++ precache-manifest.2e0cf7c53188003bddf63517c7f20be7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3dbe204896a3e1b3bcd52409797a4bfa",
+    "url": "/index.html"
+  },
+  {
+    "revision": "58bed1fa7639cf23141c",
+    "url": "/static/css/main.83bc1921.chunk.css"
+  },
+  {
+    "revision": "ac309656be3fb1279381",
+    "url": "/static/js/2.25d04bf1.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.25d04bf1.chunk.js.LICENSE"
+  },
+  {
+    "revision": "58bed1fa7639cf23141c",
+    "url": "/static/js/main.c77f9f98.chunk.js"
+  },
+  {
+    "revision": "7818d1edca579061cefa",
+    "url": "/static/js/runtime-main.4d79811d.js"
+  },
+  {
+    "revision": "45780f285426f49d4e42bd4f88acf164",
+    "url": "/static/media/logo.45780f28.jpeg"
+  }
+]);
